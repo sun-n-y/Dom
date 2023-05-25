4700d1b4aab33e48e11a3c37f29a378d04420c79
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-25 17:34:42 -0400

    get elements by id

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+//methods
 
+//get acces to node object and assign variable then style it
+const h1 = document.getElementById('title');
+
+h1.style.color = 'red';
+
+// slect element directly and apply changes
+
+document.getElementById('btn').style.background = 'blue';
+document.getElementById('btn').style.color = 'red';
+
+//refractor it to short syntax
+const btn = document.getElementById('btn');
+btn.style.backgroundColor = 'green';
+btn.style.color = 'white';
