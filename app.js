4027f5b06bf3ae416a71b8abd1f5ba6f9cb4593d
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-25 18:00:05 -0400

    get elements by tag

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 //methods
 
-//get acces to node object and assign variable then style it
-const h1 = document.getElementById('title');
+// //get elements by id
 
-h1.style.color = 'red';
+// //get acces to node object and assign variable then style it
+// const h1 = document.getElementById('title');
 
-// slect element directly and apply changes
+// h1.style.color = 'red';
 
-document.getElementById('btn').style.background = 'blue';
-document.getElementById('btn').style.color = 'red';
+// // slect element directly and apply changes
 
-//refractor it to short syntax
-const btn = document.getElementById('btn');
-btn.style.backgroundColor = 'green';
-btn.style.color = 'white';
+// document.getElementById('btn').style.background = 'blue';
+// document.getElementById('btn').style.color = 'red';
+
+// //refractor it to short syntax
+// const btn = document.getElementById('btn');
+// btn.style.backgroundColor = 'green';
+// btn.style.color = 'white';
+
+// get elements based on tag name
+
+const headings = document.getElementsByTagName('h2');
+// console.log(headings); //result is an html collection = array like object
+headings[0].style.color = 'red';
+// console.log(headings.length);
+
+const items = document.getElementsByTagName('li');
+items[1].style.color = 'orange';
+//turn items into an array
+const betterItems = [...items];
+
+betterItems.forEach(function (item) {
+  console.log(item);
+});
+
+console.log(items);
+console.log(betterItems);
