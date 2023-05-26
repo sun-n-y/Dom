979a8e24a11f9eb4e49f44c47f4aa9c8b4baf63c
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 10:51:09 -0400

    add dom text content property

---
+++ 03 more properties/app.js
@@ -0,0 +1,8 @@
+//more properties
+
+const item = document.getElementById('special');
+const value = item.nodeValue;
+console.log(item.childNodes[0].nodeValue);
+
+const easyValue = item.textContent;
+console.log(easyValue);
