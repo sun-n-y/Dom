36dc2a360eb75f5168ba6ba094f538b0e59efa2d
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 15:22:08 -0400

    add selector get element by id

---
+++ 01 getElementById/app.js
@@ -0,0 +1,9 @@
+//get element by id
+
+const h1 = document.getElementById('title');
+console.log(h1);
+
+h1.style.color = 'red';
+
+document.getElementById('btn').style.color = 'white';
+document.getElementById('btn').style.background = 'red';
