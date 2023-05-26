f6f7f91bd97175bd4714a287a0ee4888050dd7b7
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 17:46:49 -0400

    add navigate dom children

---
+++ 05 traverse dom childern/app.js
@@ -0,0 +1,12 @@
+//traverse dom childern
+
+const result = document.querySelector('#result');
+
+const allChildren = result.childNodes;
+console.log(allChildren);
+
+const children = result.children;
+console.log(children);
+
+console.log(result.firstChild);
+console.log(result.lastChild);
