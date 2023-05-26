597e2384c4df36176cdb0e189b6204226cd2cf5f
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 11:36:56 -0400

    add dom classlist property and methods

--- 03 more properties/app.js
+++ 03 more properties/app.js
@@ -7,21 +7,48 @@
 // const easyValue = item.textContent;
 // console.log(easyValue);
 
-//attributes
+// //attributes===================================================
 
-const first = document.querySelector('.first');
-const idValue = first.getAttribute('id');
+// const first = document.querySelector('.first');
+// const idValue = first.getAttribute('id');
 
-// console.log(idValue);
+// // console.log(idValue);
 
-const link = document.getElementById('link');
-const showLink = link.getAttribute('href');
-console.log(showLink);
+// const link = document.getElementById('link');
+// const showLink = link.getAttribute('href');
+// console.log(showLink);
 
-const last = link.nextElementSibling;
-last.setAttribute('class', 'first');
-last.textContent = 'i also have a class of first';
-console.log(last);
+// const last = link.nextElementSibling;
+// last.setAttribute('class', 'first');
+// last.textContent = 'i also have a class of first';
+// console.log(last);
 
-const links = document.querySelectorAll('.first');
-console.log(links);
+// const links = document.querySelectorAll('.first');
+// console.log(links);
+
+// classname and list
+
+const first = document.getElementById('first');
+const second = document.getElementById('second');
+const third = document.getElementById('third');
+
+// const classValue = first.className;
+// console.log(classValue);
+
+second.className = 'colors';
+second.className = 'text';
+
+// third.classList.add('colors');
+// third.classList.add('text');
+third.classList.add('text', 'colors');
+third.classList.remove('text');
+
+let result = third.classList.contains('colors');
+if (result) {
+  console.log(`hello`);
+} else {
+  console.log('no');
+}
+
+const classValue = third.classList;
+console.log(classValue);
