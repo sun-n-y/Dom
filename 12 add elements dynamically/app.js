015ba1ddce21393d7980d6f124ee7de093c146c8
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 19:34:43 -0400

    add elements dynamically

---
+++ 12 add elements dynamically/app.js
@@ -0,0 +1,24 @@
+// add elements and text
+
+const result = document.querySelector('#result');
+
+//create empty element
+const bodyDiv = document.createElement('div');
+//create text node
+const text = document.createTextNode('a simple body div');
+//append text node to bodyDiv
+bodyDiv.appendChild(text);
+//append bodydiv to body
+document.body.appendChild(bodyDiv);
+
+//seen only when js runs
+
+//exercise
+const heading = document.createElement('h2');
+const headingText = document.createTextNode('dynamic heading');
+
+heading.appendChild(headingText);
+heading.classList.add('blue');
+result.appendChild(heading);
+
+console.log(result.children);
