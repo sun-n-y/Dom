84de0a36f4a322b5d0c179b2af3669c569c2a58a
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 18:26:45 -0400

    add gettin gtext content within elemnet

---
+++ 09 content within element/app.js
@@ -0,0 +1,10 @@
+// content within element
+
+const item = document.getElementById('special');
+
+const value = item.nodeValue;
+
+// console.log(item.childNodes[0].nodeValue);
+
+const easyValue = item.textContent;
+console.log(easyValue);
