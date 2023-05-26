6895f21e0850c92266563dfa3005864be61b3c88
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 11:08:42 -0400

    add dom get set attribute methods

--- 03 more properties/app.js
+++ 03 more properties/app.js
@@ -1,8 +1,27 @@
-//more properties
+// //more properties
 
-const item = document.getElementById('special');
-const value = item.nodeValue;
-console.log(item.childNodes[0].nodeValue);
+// const item = document.getElementById('special');
+// const value = item.nodeValue;
+// console.log(item.childNodes[0].nodeValue);
 
-const easyValue = item.textContent;
-console.log(easyValue);
+// const easyValue = item.textContent;
+// console.log(easyValue);
+
+//attributes
+
+const first = document.querySelector('.first');
+const idValue = first.getAttribute('id');
+
+// console.log(idValue);
+
+const link = document.getElementById('link');
+const showLink = link.getAttribute('href');
+console.log(showLink);
+
+const last = link.nextElementSibling;
+last.setAttribute('class', 'first');
+last.textContent = 'i also have a class of first';
+console.log(last);
+
+const links = document.querySelectorAll('.first');
+console.log(links);
