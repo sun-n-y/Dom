5d92a695868ec29737a32af473706e340fcfea9d
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 12:22:25 -0400

    add elements property and methods

--- 03 more properties/app.js
+++ 03 more properties/app.js
@@ -26,29 +26,48 @@
 // const links = document.querySelectorAll('.first');
 // console.log(links);
 
-// classname and list
+// // classname and list=======================================
 
-const first = document.getElementById('first');
-const second = document.getElementById('second');
-const third = document.getElementById('third');
+// const first = document.getElementById('first');
+// const second = document.getElementById('second');
+// const third = document.getElementById('third');
 
-// const classValue = first.className;
+// // const classValue = first.className;
+// // console.log(classValue);
+
+// second.className = 'colors';
+// second.className = 'text';
+
+// // third.classList.add('colors');
+// // third.classList.add('text');
+// third.classList.add('text', 'colors');
+// third.classList.remove('text');
+
+// let result = third.classList.contains('colors');
+// if (result) {
+//   console.log(`hello`);
+// } else {
+//   console.log('no');
+// }
+
+// const classValue = third.classList;
 // console.log(classValue);
 
-second.className = 'colors';
-second.className = 'text';
+//adding elements dynamically ====================================
 
-// third.classList.add('colors');
-// third.classList.add('text');
-third.classList.add('text', 'colors');
-third.classList.remove('text');
+const result = document.querySelector('#result');
 
-let result = third.classList.contains('colors');
-if (result) {
-  console.log(`hello`);
-} else {
-  console.log('no');
-}
+//create emptry element
+const bodyDiv = document.createElement('div');
+// create text node
+const text = document.createTextNode('a simple body div');
+bodyDiv.appendChild(text);
 
-const classValue = third.classList;
-console.log(classValue);
+document.body.appendChild(bodyDiv);
+
+const h1 = document.createElement('h1');
+const h1text = document.createTextNode('hello');
+h1.appendChild(h1text);
+h1.classList.add('blue');
+result.appendChild(h1);
+console.log(result.children);
