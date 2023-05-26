3c695a08f58cfb54b9287dfd0d55f138e365f0f2
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-25 21:21:50 -0400

    navigate the dom childern

---
+++ 02 navigate dom/app.js
@@ -0,0 +1,12 @@
+// navigate the dom
+
+const result = document.querySelector('#result');
+const allChildern = result.childNodes;
+
+console.log(allChildern);
+
+const childern = result.children;
+console.log(childern);
+
+console.log(result.firstChild);
+console.log(result.lastChild);
