d11658f3f91232bfc6be8c55714aa66923efb852
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-26 15:30:33 -0400

    add selector get element by id

--- 01 getElementById/app.js
+++ 01 getElementById/app.js
@@ -1,9 +1,11 @@
 //get element by id
 
+//assign to variable - faster
 const h1 = document.getElementById('title');
 console.log(h1);
 
 h1.style.color = 'red';
 
+//not assigning to variable - slower
 document.getElementById('btn').style.color = 'white';
 document.getElementById('btn').style.background = 'red';
