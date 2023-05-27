30dbb88850692803c7fda66814ff84d9b8673185
sunny patel <sunnykpatel.skp@gmail.com>
2023-05-27 11:35:35 -0400

    add style vs classlist

---
+++ 18 change css in dom/app.js
@@ -0,0 +1,13 @@
+//CSS
+
+//select element
+const random = document.querySelector('.random');
+
+//took four lines with style property
+// random.style.backgroundColor = 'blue';
+// random.style.color = 'white';
+// random.style.fontSize = '3rem';
+// random.style.textTransform = 'capitalize';
+
+//using class list property
+random.classList.add('title');
